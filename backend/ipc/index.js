37618bcb765b8be76cf9f62272b704dabf9e3e15
GitMC-8383 <devp1234567891@outlook.com>
2025-09-30 21:54:49 -0500

    fix cooked

--- backend/ipc/index.js
+++ backend/ipc/index.js
@@ -1,7 +1,21 @@
-
+// IPC bridge: spawns C++ autopilot and parses stdout
+const { spawn } = require('child_process');
 
 function startAutopilot() {
-    
+    const proc = spawn('./autopilot', [], { cwd: '../../autopilot' });
+    proc.stdout.on('data', (data) => {
+        // Parse and forward telemetry
+        try {
+            const msg = JSON.parse(data.toString());
+            // TODO: emit to ws/telemetry
+            console.log('Telemetry:', msg);
+        } catch (e) {
+            console.error('Parse error:', e);
+        }
+    });
+    proc.stderr.on('data', (data) => console.error('Autopilot error:', data.toString()));
+    proc.on('close', (code) => console.log('Autopilot exited:', code));
+    return proc;
 }
 
-export default { startAutopilot };
+module.exports = { startAutopilot };
