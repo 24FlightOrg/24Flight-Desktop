47d991d3e2d97563a486fc5e4b3f42ccf92918a7
Tristannasaurus <tristanjhepburn@gmail.com>
2026-06-12 00:25:08 -0500

    topbar fix
    killed the weird blue color

---
+++ autopilot/build/CMakeFiles/autopilot.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "/home/trist/Documents/GitHub/24FlightSystem/autopilot/autopilot"
+  "/home/trist/Documents/GitHub/24FlightSystem/autopilot/autopilot.pdb"
+  "CMakeFiles/autopilot.dir/link.d"
+  "CMakeFiles/autopilot.dir/src/autopilot.cpp.o"
+  "CMakeFiles/autopilot.dir/src/autopilot.cpp.o.d"
+  "CMakeFiles/autopilot.dir/src/helpers/console.cpp.o"
+  "CMakeFiles/autopilot.dir/src/helpers/console.cpp.o.d"
+  "CMakeFiles/autopilot.dir/src/helpers/input.cpp.o"
+  "CMakeFiles/autopilot.dir/src/helpers/input.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/autopilot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
